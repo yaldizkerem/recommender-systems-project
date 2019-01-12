9f3736aab277145e2527ca990a551feaf401bcb9
Kerem Yaldız <contact@keremyaldiz.com>
2019-01-12 14:20:40 +0300

    Added coverage

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ userUser = pd.DataFrame(data=userItem.T).corr()
 itemItem = pd.DataFrame(data=itemUser.T).corr()
 
 adjacencyMatrix = pd.concat([pd.DataFrame(data=userUser), pd.DataFrame(data=data.values)], axis=1)
-adjacencyMatrix = pd.concat([adjacencyMatrix, pd.concat([pd.DataFrame(data=data.T.values), pd.DataFrame(data=itemItem)], axis=1)])
+adjacencyMatrix = pd.concat([adjacencyMatrix, pd.concat([pd.DataFrame(data=data.T.values).multiply(-1), pd.DataFrame(data=itemItem)], axis=1)])
 
 hitRates = [[],[],[],[],[],[],[],[],[],[]]
+coverageRates = [[],[],[],[],[],[],[],[],[],[]]
 
 for length in range(3):
     adjacencyMatrix = np.dot(np.dot(adjacencyMatrix, adjacencyMatrix), adjacencyMatrix)
@@ -33,13 +34,16 @@ for length in range(3):
         for index, row in enumerate(np.argsort(resultMatrix, axis=1)[:, :(-10 * top - 1):-1]):
             suggestions[data.index.values[index]] = data.columns[row].values
         hit = miss = 0
+        suggestedMovie = []
         for item in dataSample[['user', 'movie']].values:
+        suggestedMovie.append(item[1])
             try:
                 if item[1] in suggestions[item[0]]: hit+=1
                 else: miss+=1
             except:
                 pass
         hitRates[top - 1].append(hit / (float(hit) + miss) * 100)
+        coverageRates[top - 1].append(len(set(suggestedMovie))/float(len(itemItem)))
 
 from gpcharts import figure
 
@@ -51,4 +55,12 @@ for hit in hitRates:
     yValues += [hit]
 
 hitFigure.plot(xValues, yValues)
-      
+
+hitFigure = figure(title = 'Coverage Comparison', ylabel = 'Coverage')
+xValues = ['Top-N'] + range(10,101,10)
+yValues = [['Length=3', 'Length=5', 'Length=7']]
+
+for coverage in coverageRates:
+    yValues += [coverage]
+
+hitFigure.plot(xValues, yValues)
